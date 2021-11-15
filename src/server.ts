310051dbcac840b0c6bf181fe36fcb641466c3ba
Richard Ng <richardcrng@gmail.com>
2021-11-15 19:27:02 +0000

    Install CI dependencies and format files

--- src/server.ts
+++ src/server.ts
@@ -5,34 +5,36 @@ app.use(express.json()); // to parse data
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Ugh, what a boring GET request"
+    message: "Ugh, what a boring GET request",
   });
-})
+});
 
 app.post("/", (req, res) => {
   const keyCount = Object.keys(req.body).length;
   if (keyCount > 0) {
     res.json({
-      message: `A much more exciting POST request - you posted me some data which had ${keyCount} key${keyCount > 1 ? 's' : ''}`,
+      message: `A much more exciting POST request - you posted me some data which had ${keyCount} key${
+        keyCount > 1 ? "s" : ""
+      }`,
     });
   } else {
     res.json({
-      message: "This is a promising POST request - maybe attach some data, too?"
-    })
+      message:
+        "This is a promising POST request - maybe attach some data, too?",
+    });
   }
 });
 
 app.post<{}, {}, { numbers: number[] }>("/sum", (req, res) => {
   let total = 0;
-  for (let n of req.body.numbers) {
+  for (const n of req.body.numbers) {
     total += n;
   }
   res.json({
-    original: `${req.body.numbers.join(' + ')}`,
-    result: total
+    original: `${req.body.numbers.join(" + ")}`,
+    result: total,
   });
 });
-
 
 // using 4000 by convention, but could be changed
 const PORT_NUMBER = 4000;
